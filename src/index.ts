0b2f40e0416e775985ea51f736a9b11bfc4deb53
Simon Skinner <vultuk28@gmail.com>
2021-05-05 22:47:13 +0100

    Work

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { Application } from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
+import {Application} from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import {GraphQLObjectType, GraphQLSchema} from 'graphql';
 
-import { Settings } from './Types/settings';
+import {Settings} from './Types/settings';
 
 export * as GraphQL from 'graphql';
 
-export default (fields: any, settings: Settings) => (app: Application) => {
+export default (fields: any, mutations: any, settings: Settings) => (app: Application) => {
   const rootSchema = new GraphQLSchema({
     query: new GraphQLObjectType({
       name: 'RootQuery',
       description: 'Root Query',
       fields: () => fields,
     }),
+    mutation: mutations,
   });
 
   app.get(settings.path, graphqlHTTP({ schema: rootSchema, graphiql: settings.interface || true }));
