5a171d5517cb78e02724e70b5cd8279eeab42bcf
Simon Skinner <simon@vult.uk>
2021-05-05 12:27:31 +0100

    Work

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { Application } from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { GraphQLSchema } from 'graphql';
+import {Application} from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import {GraphQLObjectType, GraphQLSchema} from 'graphql';
 
-import { Settings } from './Types/settings';
+import {Settings} from './Types/settings';
 
 export * as GraphQL from 'graphql';
 
-export default (schema: GraphQLSchema, settings: Settings) => (app: Application) => {
-  app.get(settings.path, graphqlHTTP({ schema, graphiql: settings.interface || true }));
-  app.post(settings.path, graphqlHTTP({ schema, graphiql: false }));
+export default (fields: any, settings: Settings) => (app: Application) => {
+  const rootSchema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'RootQuery',
+      description: 'Root Query',
+      fields: () => fields,
+    }),
+  });
+
+  app.get(settings.path, graphqlHTTP({ schema: rootSchema, graphiql: settings.interface || true }));
+  app.post(settings.path, graphqlHTTP({ schema: rootSchema, graphiql: false }));
 };
