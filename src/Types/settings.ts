330f9db79321bcf9784b6b6832fdd9f3f14d00c6
Simon Skinner <simon@vult.uk>
2021-05-05 08:16:20 +0100

    first commit

---
+++ src/Types/settings.ts
@@ -0,0 +1,4 @@
+export type Settings = {
+  path: string;
+  interface?: boolean;
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Application} from 'express';
+import {graphqlHTTP} from 'express-graphql';
+
+import {Settings} from './Types/settings';
+
+export default (app: Application, schema: any, settings: Settings) =>
+  app.use(settings.path, graphqlHTTP({ schema, graphiql: settings.interface || true }));
