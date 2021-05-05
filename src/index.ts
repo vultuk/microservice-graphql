99d85cd1a2313d222d488b151d4df3f20f5d0a7e
Simon Skinner <simon@vult.uk>
2021-05-05 12:27:34 +0100

    0.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {Application} from 'express';
-import {graphqlHTTP} from 'express-graphql';
-import {GraphQLObjectType, GraphQLSchema} from 'graphql';
+import { Application } from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 
-import {Settings} from './Types/settings';
+import { Settings } from './Types/settings';
 
 export * as GraphQL from 'graphql';
 
