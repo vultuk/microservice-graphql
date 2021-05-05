3b76a041b8debb2e564efe536f5839239e6d9a2a
Simon Skinner <simon@vult.uk>
2021-05-05 09:20:28 +0100

    Work

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { Application } from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { GraphQLSchema } from 'graphql';
+import {Application} from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import {GraphQLSchema} from 'graphql';
 
-import { Settings } from './Types/settings';
+import {Settings} from './Types/settings';
 
 export * as GraphQL from 'graphql';
 
-export default (schema: GraphQLSchema, settings: Settings) => (app: Application) =>
+export default (schema: GraphQLSchema, settings: Settings) => (app: Application) => {
   app.get(settings.path, graphqlHTTP({ schema, graphiql: settings.interface || true }));
+  app.post(settings.path, graphqlHTTP({ schema, graphiql: false }));
+};
